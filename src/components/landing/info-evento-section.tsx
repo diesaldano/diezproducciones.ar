c88d402c11138262cf45e6e4e472fc7ea9e3fff4
Diez Producciones <diesaldano@gmail.com>
2026-04-09 19:11:41 -0300

    LP-007: fix hydration mismatch - add inline-block to emoji spans

--- src/components/landing/info-evento-section.tsx
+++ src/components/landing/info-evento-section.tsx
@@ -64,7 +64,7 @@ export function InfoEventoSection() {
                     className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 object-contain"
                   />
                 ) : (
-                  <span className="text-5xl sm:text-6xl md:text-7xl">
+                  <span className="text-5xl sm:text-6xl md:text-7xl inline-block">
                     {item.icon}
                   </span>
                 )}
